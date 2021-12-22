1785bc13127dbec3ae6542b8c5aebd9b0131fc05
apease <apease@articulatesoftware.com>
2021-12-21 17:10:08 -0800

    remove obsolete code

--- src/java/tptp_parser/TPTPFormula.java
+++ src/java/tptp_parser/TPTPFormula.java
@@ -24,32 +24,11 @@ public class TPTPFormula {
     /** ***************************************************************
      */
     public TPTPFormula() {
-
     }
 
     /** ***************************************************************
     */
     public TPTPFormula(TptpParser.Annotated_formulaContext item, int id) {
-
-      /*
-      this.item = item;
-      this.id = id;
-      type = TPTPParser.getType(item);
-      parents = new ArrayList();
-      children = new ArrayList();
-
-      if (item.getKind() == SimpleTptpParserOutput.TopLevelItem.Kind.Formula) {
-          SimpleTptpParserOutput.AnnotatedFormula AF = ((SimpleTptpParserOutput.AnnotatedFormula) item);
-          annotations = AF.getAnnotations();
-      }
-      else if (item.getKind() == SimpleTptpParserOutput.TopLevelItem.Kind.Clause) {
-          SimpleTptpParserOutput.AnnotatedClause AC = ((SimpleTptpParserOutput.AnnotatedClause) item);
-          annotations = AC.getAnnotations();
-      }
-      if (annotations != null) {
-          source = annotations.getSource();
-      }
-       */
     }
 
     /** ***************************************************************
@@ -76,51 +55,6 @@ public class TPTPFormula {
     public String getFormula () {
 
         return formula;
-    }
-
-    /** ***************************************************************
-    * given a cnf formula, turn it into an fof formula (universally quantify all variables)
-    */
-    public String fofify () {
-
-      String UniversalQuantifier = "!";
-      StringBuffer res = new StringBuffer();
-
-      /*
-
-      if (item.getKind() == SimpleTptpParserOutput.TopLevelItem.Kind.Formula) {
-          SimpleTptpParserOutput.AnnotatedFormula AF = ((SimpleTptpParserOutput.AnnotatedFormula)item);
-          res.append("fof(");
-          res.append(TPTPParser.getName(item) + ",");
-          res.append(AF.getRole().toString());
-          res.append(",(" + "\n");
-          res.append("    " + AF.getFormula().toString(4));
-          res.append(" )).");
-      }
-      else if (item.getKind() == SimpleTptpParserOutput.TopLevelItem.Kind.Clause) {
-          SimpleTptpParserOutput.AnnotatedClause AC = ((SimpleTptpParserOutput.AnnotatedClause)item);
-          ArrayList<String> variables = new ArrayList();
-          variables = TPTPParser.identifyClauseVariables(AC.getClause(), variables);
-          res.append("fof(");
-          res.append(TPTPParser.getName(item) + ",");
-          //      res += (conjecture) ? AC.getRole().toString() : "axiom";
-          res.append(AC.getRole().toString());
-          res.append(", (" + "\n");
-          if (!variables.isEmpty()) {
-              res.append("    " + UniversalQuantifier + " [");
-              res.append(variables.get(0));
-              for (int i = 1; i < variables.size(); i++) {
-                  res.append("," + variables.get(i));
-              }
-              res.append("] : " + "\n");
-          }
-          res.append((!variables.isEmpty()) ? "      ( " : "    ");
-          res.append((!variables.isEmpty()) ? AC.getClause().toString(8) : AC.getClause().toString(4));
-          res.append((!variables.isEmpty()) ? " )" : "");
-          res.append(" )).");
-      }
-       */
-      return res.toString();
     }
 
     /** ***************************************************************
